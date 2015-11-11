924b424fa0b1807009af0e6ba6b9a72054f01eaa
Lena Plaksina <lena.plaksina@gmail.com>
2015-11-12 12:05:47 +1300

    Update dependencies

--- config/app.php
+++ config/app.php
@@ -144,6 +144,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\ViewComposerServiceProvider::class,
 
         Collective\Html\HtmlServiceProvider::class,
         Codesleeve\LaravelStapler\Providers\L5ServiceProvider::class,
