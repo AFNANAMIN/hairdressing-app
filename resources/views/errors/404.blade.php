9a97babf7099a20c8034c0970f3234c835316af6
Lena Plaksina <lena.plaksina@gmail.com>
2015-11-19 12:29:18 +1300

    Add 404 error layout

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.master')
+
+@section('content')
+	<div class="contact-header"> <!-- Main jumbotron for a primary marketing message or call to action -->
+        <div class="jumbotron">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <p class="subheading">Whoops!<p>
+                    </div>
+                </div>
+            </div>
+        </div> <!-- / container -->
+    </div> <!-- / jumbotron -->
+
+    <div class="container padded-div">
+
+    	<p class="text-center subheading">Something went wrong.</p>
+
+    	<div class="row">
+    		<div class="col-sm-12">
+    			<p>It looks like the page you're looking for doesn't exist. The link you followed to get here might be out of date or broken.</p>
+                <p>Go back and try again.</p>
+    		</div>
+    	</div>
+
+    </div> <!-- /container -->
+@endsection
